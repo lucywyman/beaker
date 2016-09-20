0e2b011b49558c5e253fc6bc65c0319a2d3d8094
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-09-20 18:25:21 +0200

    (BKR-942) let beaker use either old pkg or new pkgng tools for package mgmt (freebsd)

--- spec/beaker/host/freebsd/pkg_spec.rb
+++ spec/beaker/host/freebsd/pkg_spec.rb
@@ -27,11 +27,16 @@ module Beaker
     let (:opts)     { @opts || {} }
     let (:logger)   { double( 'logger' ).as_null_object }
     let (:instance) { FreeBSDPkgTest.new(opts, logger) }
+    let(:cond) do
+      'TMPDIR=/dev/null ASSUME_ALWAYS_YES=1 PACKAGESITE=file:///nonexist pkg info -x "pkg(-devel)?\\$" > /dev/null 2>&1'
+    end
 
     context "install_package" do
+      let(:st) { 'pkg install -y rsync' }
+      let(:sf) { 'pkg_add -r rsync' }
 
       it "runs the correct install command" do
-        expect( Beaker::Command ).to receive(:new).with('pkg install -y rsync', [], {:prepend_cmds=>nil, :cmdexe=>false}).and_return('')
+        expect( Beaker::Command ).to receive(:new).with("/bin/sh -c 'if #{cond}; then #{st}; else #{sf}; fi'", [], {:prepend_cmds=>nil, :cmdexe=>false}).and_return('')
         expect( instance ).to receive(:exec).with('', {}).and_return(generate_result("hello", {:exit_code => 0}))
         instance.install_package('rsync')
       end
@@ -39,9 +44,11 @@ module Beaker
     end
 
     context "check_for_package" do
+      let(:st) { 'pkg info rsync' }
+      let(:sf) { 'pkg_info -Iqx rsync 2> /dev/null || true' }
 
       it "runs the correct checking command" do
-        expect( Beaker::Command ).to receive(:new).with('pkg info rsync', [], {:prepend_cmds=>nil, :cmdexe=>false}).and_return('')
+        expect( Beaker::Command ).to receive(:new).with("/bin/sh -c 'if #{cond}; then #{st}; else #{sf}; fi'", [], {:prepend_cmds=>nil, :cmdexe=>false}).and_return('')
         expect( instance ).to receive(:exec).with('', {}).and_return(generate_result("hello", {:exit_code => 0}))
         instance.check_for_package('rsync')
       end
